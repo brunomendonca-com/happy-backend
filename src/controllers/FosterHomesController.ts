bf2908bd9bed5bd08df1d2360b09a8b930edbf87
Bruno Mendonca <me@brunomendonca.com>
2020-10-13 20:01:47 -0600

    Add validation, cors, and error handling

--- src/controllers/FosterHomesController.ts
+++ src/controllers/FosterHomesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import FosterHome from '../models/FosterHome';
 import fosterhomeView from "../views/fosterhomes_views";
+import * as Yup from 'yup';
 
 export default {
 
@@ -42,7 +43,7 @@ export default {
       return { path: image.filename }
     })
 
-    const fosterHome = fosterHomesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -51,7 +52,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
-    })
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
+    });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const fosterHome = fosterHomesRepository.create(data)
 
     await fosterHomesRepository.save(fosterHome);
 
